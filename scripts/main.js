057ab231806d9c9d6cd6f39e88170b34d7184fa6
phatommobb <nickoluoch1130@gmail.com>
2024-10-31 20:12:55 +0300

    handle scroll

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,10 @@ const topBar = document.querySelector("#top-bar");
 
 //Handle top bar scroll
 const handleScroll = () => {
-    
+  const aTop = window.scrollY === 0;
+  topBar.classList.toggle('visible-bar',aTop);
+  topBar.classList.toggle('hidden-bar',!aTop)  
 }
+
+
+window.addEventListener("scroll",handleScroll);
