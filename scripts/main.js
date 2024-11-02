22a08515c9b89da024f8bccec0d2005694090c35
phatommobb <nickoluoch1130@gmail.com>
2024-11-02 14:08:20 +0300

    color selection

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,9 @@
 const topBar = document.querySelector("#top-bar");
+const exteriorColorSelection = document.querySelector('#exterior-buttons');
+const interiorColorSelection = document.querySelector('#interior-buttons');
+const exteriorImage = document.querySelector('#exterior-image');
+const interiorImage = document.querySelector('#interior-image');
+
 
 //Handle top bar scroll
 const handleScroll = () => {
@@ -7,5 +12,5 @@ const handleScroll = () => {
   topBar.classList.toggle('hidden-bar',!aTop)  
 }
 
-
+//Event listernors
 window.addEventListener("scroll",handleScroll);
