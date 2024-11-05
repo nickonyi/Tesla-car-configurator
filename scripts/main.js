f784bf668a15c555af2963e49cd2aac154e2d4a6
phatommobb <nickoluoch1130@gmail.com>
2024-11-05 18:22:00 +0300

    full self driving

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,9 @@ const interiorImage = document.querySelector('#interior-image');
 const wheelButtonSection = document.querySelector('#wheel-buttons');
 const performanceBtn = document.querySelector('#performance-btn');
 const totalPriceElement = document.querySelector('#total-price');
+const fullSelfDrivingCheckBox = document.querySelector('#full-self-driving-checkbox');
+
+
 
 const basePrice = 52490;
 let currentPrice = basePrice;
